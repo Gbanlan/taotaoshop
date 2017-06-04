c7abe4c08055141cf3eb2179147f2741a275d0de
seven-mz <zhaomingyang90@163.com>
2017-06-04 18:14:58 +0800

    123
    123

---
+++ taotao-rest/src/main/java/com/taotao/rest/service/impl/ItemServiceImpl.java
@@ -0,0 +1,83 @@
+package com.taotao.rest.service.impl;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.mapper.TbItemDescMapper;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.mapper.TbItemParamItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
+import com.taotao.pojo.TbItemParamItem;
+import com.taotao.pojo.TbItemParamItemExample;
+import com.taotao.pojo.TbItemParamItemExample.Criteria;
+import com.taotao.rest.service.ItemService;
+
+/**
+ * 商品信息管理Service
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.com</p> 
+ * @author	入云龙
+ * @date	2015年9月12日上午10:18:39
+ * @version 1.0
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
+	
+	@Autowired
+	private TbItemParamItemMapper itemParamItemMapper;
+	
+	@Value("${REDIS_ITEM_KEY}")
+	private String REDIS_ITEM_KEY;
+	@Value("${REDIS_ITEM_EXPIRE}")
+	private Integer REDIS_ITEM_EXPIRE;
+	
+	
+	@Override
+	public TaotaoResult getItemBaseInfo(long itemId) {
+		
+		//根据商品id查询商品信息
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		//使用TaotaoResult包装一下
+		return TaotaoResult.ok(item);
+	}
+
+	@Override
+	public TaotaoResult getItemDesc(long itemId) {
+
+		//创建查询条件
+		TbItemDesc itemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+		
+		return TaotaoResult.ok(itemDesc);
+	}
+
+	@Override
+	public TaotaoResult getItemParam(long itemId) {
+		
+		//根据商品id查询规格参数
+		//设置查询条件
+		TbItemParamItemExample example = new TbItemParamItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andItemIdEqualTo(itemId);
+		//执行查询
+		List<TbItemParamItem> list = itemParamItemMapper.selectByExampleWithBLOBs(example);
+		if (list != null && list.size()>0) {
+			TbItemParamItem paramItem = list.get(0);
+			return TaotaoResult.ok(paramItem);
+		}
+		return TaotaoResult.build(400, "无此商品规格");
+	}
+
+}
